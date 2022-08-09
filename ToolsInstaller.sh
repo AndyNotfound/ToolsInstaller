58b9e300aa727e568e1e88d51eb6d055dc357cfb
AndyNotfound <Andy.Notfound@gmail.com>
2022-08-09 17:07:36 +0700

    Updated, modified all of the packages installer

--- ToolsInstaller.sh
+++ ToolsInstaller.sh
@@ -1,29 +1,36 @@
 #!/data/data/com.termux/files/usr/bin/bash
-# MR.A_404 Installer Tools
+
+
+######################################################################################
+## Disclaimer : This tool is made for fun, use everything at your own risk, Okay ?  ##
+##   Everybody is permitted to edit, distribute this tools under GPL 2.0 License    ##
+## Some of the tools are probably outdated and I'll try my best to update them,kay? ##
+######################################################################################
 
 clear
+
+## Colors Variable
 green='\e[0;34m'
 white='\e[1;37m'
 red='\e[1;31m'
 
-###################################################
-# Detecting CTRL + C Key
-###################################################
-trap ctrl_c INT
 
+## Detecting CTRL + C Key and then exiting
+trap ctrl_c INT
 ctrl_c() {
     clear
     echo -e $red"[#]> ( Ctrl + C ) Detected, Exit Now "
     sleep 1
     echo ""
-    echo -e $white"[#]> Good Bye MR.A_404 :* "
+    echo -e $white"[#]> Goodbye Dear User :* "
     sleep 1
     echo ""
-    echo -e $red"[#]> MR.A_404 kick you ... "
+    echo -e $red"[#]> Exterminating the session ... "
     read enter
     exit
 }
 
+## Listing all Avaliable Option : Updated per 09 August 20222
 echo -e $red"___           _        _ _             _____           _"
 echo -e $white"|_ _|_ __  ___| |_ __ _| | | ___ _ __  |_   _|__   ___ | |___"
 echo -e $red"| ||  _ \/ __| __/ _  | | |/ _ \  __|   | |/ _ \ / _ \| / __|"
@@ -32,8 +39,7 @@ echo -e $red"|___|_| |_|___/\__\__,_|_|_|\___|_|      |_|\___/ \___/|_|___/"
 echo " "
 echo " "
 echo -e $red"-------------------------------------------------"
-echo -e $red"| $white Installer Tools Coded By MR.A_404            $red |"
-echo -e $red"| $white Greetz : Ghost Hunter Illusion - HexolzWolf  $red |"
+echo -e $red"| $white Installer Tools Coded By Andy                $red |"
 echo -e $red"| $white Contact : Andy.Notfound@gmail.com            $red |"
 echo -e $red"-------------------------------------------------"
 echo -e $white" 01) Red Hawk"
@@ -61,30 +67,29 @@ echo -e $green" 22) Sudo Termux"
 echo -e $green" 23) Air Cracking"
 echo -e $green" 24) Joomla Scan"
 echo -e $green" 25) Bing Ip2 Hosts"
-
-
-echo -e $green" 36) Blue Pot"
-echo -e $green" 37) Honey Pot"
-echo -e $green" 38) Bot Auto Deface 1"
-echo -e $green" 39) Bot Auto Deface 2"
-echo -e $green" 40) Mail Sender "
-echo -e $green" 41) Wordpress Brute Force"
-echo -e $green" 42) Oh-myzsh Theme"
-echo -e $green" 43) Instagram Bot"
-echo -e $green" 44) Fuck Society"
-echo -e $green" 45) Cms Scanner"
-echo -e $green" 46) Information Gathering"
-echo -e $green" 47) com_fabrik Xploiter"
-echo -e $green" 48) com foxcontact Xploiter"
-echo -e $green" 49) Gmail Brute Force"
-echo -e $green" 50) ezXploit"
-echo -e $green" 51) Spammer Grab"
-echo -e $green" 52) Spammer Call Tokopedia"
-echo -e $green" 53) The Fat Rat"
-echo -e $green" 54) IP Geolocation"
-echo -e $green" 55) exit"
+echo -e $green" 26) Honey Pot"
+echo -e $green" 27) Bot Auto Deface 1"
+echo -e $green" 28) Bot Auto Deface 2"
+echo -e $green" 29) Mail Sender "
+echo -e $green" 30) Oh-myzsh Theme"
+echo -e $green" 31) Cms Scanner"
+echo -e $green" 32) Information Gathering"
+echo -e $green" 33) com_fabrik Xploiter"
+echo -e $green" 34) com foxcontact Xploiter"
+echo -e $green" 35) ezXploit"
+echo -e $green" 36) Spammer Grab"
+echo -e $green" 37) Spammer Call Tokopedia"
+echo -e $green" 38) The Fat Rat"
+echo -e $green" 39) IP Geolocation"
+echo -e $green" 40) exit"
 echo -e $white""
+
+## Prompting User to Choose and store it inside a variable
 read -p "[MR.A_404@choose]> " userChoice;
+
+#############################################################
+## List of things that will be run according to user input ##
+#############################################################
 
 ## RED HAWK
 if [ $userChoice = 01 ] || [ $userChoice = 1 ]
@@ -452,267 +457,218 @@ if [ $userChoice = 25 ]
     echo -e $red" done installed..."
 fi
 
-if [ $userChoice = 36 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install git
-git clone git://git.kali.org/packages/bluepot.git
-echo -e $red" done installed..."
-fi
-
-if [ $userChoice = 37 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install git && apt-get install php
-git clone https://github.com/whackashoe/php-spam-mail-honeypot.git
-echo -e $red" done installed..."
-fi
-
-if [ $userChoice = 38 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install git
-apt-get install wget
-apt-get install perl
-apt-get install unzip
-git clone https://github.com/mrcakil/bot.git
-cd bot
-unzip bot.zip
-cd xploit
-chmod 777 bot.pl
-echo -e $red"NB : /bot/xploit/bot.pl"
-echo -e $red" done installed..."
-fi
-
-if [ $userChoice = 39 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install git && apt-get install perl
-git clone https://github.com/Moham3dRiahi/XAttacker
-cd XAttacker
-chmod 777 XAttacker.pl
-echo -e $red" done installed..."
-fi
-
-if [ $userChoice = 40 ] || [ $userChoice = 40 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install php5-cli
-curl -sS https://getcomposer.org/installer | php
-chmod +x composer.phar
-sudo mv composer.phar /usr/bin/composer
-git clone https://github.com/pedro-stanaka/mailer-cli.git
-echo -e $red" NB : usage php sendmail.php notification:mailer <email> <subject> <body>; "
-echo -e $red" or "
-echo -e $red" php sendmail.php --help "
-echo -e $red" done installed... "
-fi
-
-if [ $userChoice = 41 ] || [ $userChoice = 41 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install python2
-pip install request
-git clone https://github.com/atarantini/wpbf
-echo -e $red" done installed..."
-fi
-
-if [ $userChoice = 42 ] || [ $userChoice = 42 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
-~/.termux/colors.sh
-echo -e $red" NB : cara ganti warna nya ~/.termux/colors.sh "
-echo -e $red" Done Installed..."
-fi
-
-if [ $userChoice = 43 ] || [ $userChoice = 43 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-pkg install python2
-apt-get install git
-apt-get install nano
-git clone https://github.com/instabot-py/instabot.py
-echo -e $red" Done Installed..."
-echo -e $red" Please wait... "
-sleep 1
-cd instabot.py
-echo -e $red"NB : ketik nano example.py "
-fi
-
-if [ $userChoice = 44 ] || [ $userChoice = 44 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-pkg install python
-apt-get install git
-git clone https://github.com/Manisso/fsociety
-echo -e $red" Done Installed..."
-echo -e $red" Please wait... "
-sleep 1
-cd fsociety
-echo -e $red" python fsociety.py "
-fi
-
-if [ $userChoice = 45 ] || [ $userChoice = 45 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-pkg install python
-apt-get install git
-git clone https://github.com/Dionach/CMSmap.git
-sleep 1
-cd CMSmap
-echo -e $red"Done Installed..."
-echo -e $red" NB : cmsmap.py -t <URL> "
-fi
-
-if [ $userChoice = 46 ] || [ $userChoice = 46 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-pkg install python
-apt-get install git
-git clone https://github.com/m4ll0k/Infoga.git infoga
-sleep 1
-cd infoga
-pip install -r req
-echo -e $red"Done Installed..."
-echo -e $red"NB : python infoga.py "
-fi
-
-if [ $userChoice = 47 ] || [ $userChoice = 47 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install wget
-apt-get install php
-wget https://pastebin.com/raw/LDvFvtUD -O com_fabrik.php
-sleep 1
-echo -e $red"Done Installed..."
-echo -e $red" NB : php com_fabrik.php target.txt "
-fi
-
-if [ $userChoice = 48 ] || [ $userChoice = 48 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install wget
-apt-get install php
-wget https://pastebin.com/raw/EAtSir5V -O com_foxcontact.php
-sleep 1
-echo -e $red"Done Installed..."
-echo -e $red" NB : php com_foxcontact.php target.txt "
-fi
-
-if [ $userChoice = 49 ] || [ $userChoice = 49 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install git
-git clone https://github.com/JamesAndresCM/Brute_force_gmail
-sleep 1
-echo -e $red"Done Installed..."
-echo -e $red" NB : python2.7 brute_force_gmail.py example@gmail.com PATH_TO_DICTIONARY "
-fi
-
-if [ $userChoice = 50 ] || [ $userChoice = 50 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install git
-git clone https://github.com/rand0m1ze/ezsploit
-sleep 1
-echo -e $red" Done Installed..."
-fi
-
-if [ $userChoice = 51 ] || [ $userChoice = 51 ]
-then
-clear
-echo -e $green" installing... "
-sleep 1
-apt-get install git
-apt-get install python2
-pip install requests
-git clone https://github.com/p4kl0nc4t/Spammer-Grab/
-sleep 1
-echo -e $red" Done Installed..."
-fi
-
-if [ $userChoice = 52 ] || [ $userChoice = 52 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install git
-apt-get install unzip
-apt-get install php
-git clone https://github.com/mrcakil/spam
-cd spam
-unzip toko-pedia.zip
-sleep 1
-echo -e $red" Done Installed..."
-fi
-
-if [ $userChoice = 53 ] || [ $userChoice = 53 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install git
-git clone https://github.com/Screetsec/TheFatRat.git
-cd TheFatRat
-chmod +x setup.sh && ./setup.sh
-sleep 1
-echo -e $red" Done Installed..."
-fi
-
-if [ $userChoice = 54 ] || [ $userChoice = 54 ]
-then
-clear
-echo -e $green" installing..."
-sleep 1
-apt-get install git
-apt install python2
-git clone https://github.com/maldevel/IPGeolocation.git
-cd IPGeolocation
-chmod +x ipgeolocation.py
-pip install -r requirements.txt
-sleep 1
-echo -e $red" Done Installed..."
-fi
-
-
-if [ $userChoice = 55 ] || [ $userChoice = 55 ]
+## Honey Pot
+if [ $userChoice = 26 ]
     then
-    echo -e $green" contact : Andy.Notfound@gmail.com "
+    clear
+    echo -e $green" installing..."
     sleep 1
-    echo -e $red" fb : https://www.facebook.com/Tuan.A.404 "
+    apt-get install git && apt-get install php
+    git clone https://github.com/whackashoe/php-spam-mail-honeypot.git
+    clear
+    echo -e $red" done installed..."
+fi
+
+##Auto Deface One
+if [ $userChoice = 27 ]
+    then
+    clear
+    echo -e $green" installing..."
     sleep 1
-    echo -e $red" Bye MR.A_404 :* "
+    apt-get install git wget perl unzip
+    git clone https://github.com/mrcakil/bot.git
+    cd bot
+    unzip bot.zip
+    cd xploit
+    chmod 777 bot.pl
+    echo -e $red" done installed..."
+    clear
+fi
+
+## Auto Deface Two
+if [ $userChoice = 28 ]
+    then
+    clear
+    echo -e $green" installing..."
+    sleep 1
+    apt-get install git && apt-get install perl
+    git clone https://github.com/Moham3dRiahi/XAttacker.git
+    cd XAttacker
+    chmod +x termux-install.sh
+    bash termux-install.sh
+    clear
+    echo -e $red" done installed..."
+fi
+
+## Mail Sender
+if [ $userChoice = 29 ]
+    then
+    clear
+    echo -e $green" installing..."
+    sleep 1
+    apt-get install php5-cli
+    curl -sS https://getcomposer.org/installer | php
+    chmod +x composer.phar
+    sudo mv composer.phar /usr/bin/composer
+    git clone https://github.com/pedro-stanaka/mailer-cli.git
+    clear
+    echo -e $red" done installed... "
+fi
+
+
+## Oh-myzsh Theme
+if [ $userChoice = 30 ]
+    then
+    clear
+    echo -e $green" installing..."
+    sleep 1
+    sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+    ~/.termux/colors.sh
+    echo -e $red" Done Installed..."
+    clear
+fi
+
+## CMS Map Scanner
+if [ $userChoice = 31 ]
+    then
+    clear
+    echo -e $green" installing..."
+    sleep 1
+    pkg install python
+    apt-get install git
+    git clone https://github.com/Dionach/CMSmap.git
+    sleep 1
+    cd CMSmap
+    clear
+    echo -e $red"Done Installed..."
+fi
+
+## Information gathering
+if [ $userChoice = 32 ]
+    then
+    clear
+    echo -e $green" installing..."
+    sleep 1
+    git clone https://github.com/m4ll0k/Infoga.git
+    cd Infoga
+    python setup.py install
+    python infoga.py
+    clear
+    echo -e $red"Done Installed..."
+fi
+
+## Com_FavrikXploiter
+if [ $userChoice = 33 ]
+    then
+    clear
+    echo -e $green" installing..."
+    sleep 1
+    apt-get install wget
+    apt-get install php
+    wget https://pastebin.com/raw/LDvFvtUD -O com_fabrik.php
+    sleep 1
+    clear
+    echo -e $red"Done Installed..."
+fi
+
+## Com_FocContact
+if [ $userChoice = 34 ]
+    then
+    clear
+    echo -e $green" installing..."
+    sleep 1
+    apt-get install wget
+    apt-get install php
+    wget https://pastebin.com/raw/EAtSir5V -O com_foxcontact.php
+    sleep 1
+    clear
+    echo -e $red"Done Installed..."
+fi
+
+## EzXploit
+if [ $userChoice = 35 ]
+    then
+    clear
+    echo -e $green" installing..."
+    sleep 1
+    apt-get install git
+    git clone https://github.com/rand0m1ze/ezsploit
+    cd ezsploit
+    sleep 1
+    clear
+    echo -e $red" Done Installed..."
+fi
+
+## Spammer Grab
+if [ $userChoice = 36 ]
+    then
+    clear
+    echo -e $green" installing... "
+    sleep 1
+    apt-get install git python3
+    pip install requests
+    git clone https://github.com/p4kl0nc4t/Spammer-Grab/
+    cd Spammer-Grab
+    sleep 1
+    clear
+    echo -e $red" Done Installed..."
+fi
+
+## Tokopedia Spammer
+if [ $userChoice = 37 ]
+    then
+    clear
+    echo -e $green" installing..."
+    sleep 1
+    apt-get install git
+    apt-get install unzip
+    apt-get install php
+    git clone https://github.com/mrcakil/spam
+    cd spam
+    unzip toko-pedia.zip
+    sleep 1
+    clear
+    echo -e $red" Done Installed..."
+fi
+
+## The Fat Rat
+if [ $userChoice = 38 ]
+    then
+    clear
+    echo -e $green" installing..."
+    sleep 1
+    apt-get install git
+    git clone https://github.com/Screetsec/TheFatRat.git
+    cd TheFatRat
+    ./update && chmod +x setup.sh && ./setup.sh
+    sleep 1
+    clear
+    echo -e $red" Done Installed..."
+fi
+
+## IP Geolocation
+if [ $userChoice = 39 ]
+    then
+    clear
+    echo -e $green" installing..."
+    sleep 1
+    apt-get install git
+    apt install python3 pip3
+    git clone https://github.com/maldevel/IPGeoLocation
+    pip3 install -r requirements.txt --user
+    sleep 1
+    clear
+    echo -e $red" Done Installed..."
+fi
+
+## Exit option
+if [ $userChoice = 40 ]
+    then
+    echo -e $green" Contact creator at Andy.Notfound@gmail.com "
+    sleep 1
+    echo -e $red" Use everything at your own risk!"
+    sleep 1
+    echo -e $red" Goodbye Dear :* "
     sleep 1
     exit
 fi
